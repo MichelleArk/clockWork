cf6427f127debe87d0040ba48e8afbbf2ab59358
Michelle Arkhangorodsky <m.arkhangorodsky@gmail.com>
2015-07-06 01:27:23 -0400

    Added regex to obtain number from innerHTML and outputString formatting.

--- content.js
+++ content.js
@@ -1,19 +1,17 @@
 var x = document.getElementsByClassName("price");
 for(var i = 0; i < x.length; i++){
         var str = x[i].innerHTML;
-        var pos = str.indexOf("$");
-        var res = str.slice(pos+1);
-        while(res[0] == " "){
-                res = res.slice(1);
-        }
-        res = res.trim();
-        res = res.replace(",","");
-    
-        var hourlyWage = 25;
+        var res = str.replace(",","");
+
+	var regex = /[0-9]+(\.\d{2})?/;
+	var results = res.match(regex);    	
+	res = results[0];
+
+	var hourlyWage = 25;
         var workingHours = 7.5;
         var dailyWage = hourlyWage * workingHours;
         var yearlyWage = dailyWage * 251;
-    
+    	var minuteWage = hourlyWage / 60;
         var price = Number(res);
 
         // check string converted into a real price value
@@ -29,11 +27,21 @@ for(var i = 0; i < x.length; i++){
             price = price%dailyWage;
 
             numHours = Math.floor(price/hourlyWage);
-            
-            var outputString = numHours + " Hours";
+            price = price%hourlyWage;
+
+	    numMinutes = Math.floor(price/minuteWage);
+ 
+	    var minutesString = numMinutes + " Minutes";
+            var hoursString = numHours + " Hours";
+
+	    var outputString;
             if(numDays != 0){
-                outputString = numDays + " Day(s) and " + outputString;
-            }
+                outputString = numDays + " Day(s) and " + hoursString;
+            }else if(numHours != 0){
+	    	outputString = hoursString + " and " + minutesString;
+	    }else{
+	    	outputString = minutesString;
+	    }
 
             x[i].innerHTML = outputString;
         }
