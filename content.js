9a46dfa83eadd520235510e84839551b3fe6acfe
Michelle Arkhangorodsky <m.arkhangorodsky@gmail.com>
2015-07-05 22:18:46 -0400

    Initial commit

---
+++ content.js
@@ -0,0 +1,39 @@
+var x = document.getElementsByClassName("price");
+for(var i = 0; i < x.length; i++){
+	var str = x[i].innerHTML;
+    	var pos = str.indexOf("$");
+    	var res = str.slice(pos+1);
+    	while(res[0] == " "){
+		res = res.slice(1);
+    	}
+    	
+	res = res.trim();
+    	res = res.replace(",","");
+    
+    	var hourlyWage = 25;
+    	var workingHours = 7.5;
+    	var dailyWage = hourlyWage * workingHours;
+    	var yearlyWage = dailyWage * 251;
+    
+    	var price = Number(res);
+
+    	var numYears;
+    	var numDays;
+    	var numHours;
+    
+    	if(price%yearlyWage){
+		numYears = Math.floor(price/yearlyWage);
+		price = price%yearlyWage;
+    	}
+    	
+	if(price%dailyWage){
+		numDays = Math.floor(price/dailyWage);
+		price = price%dailyWage;
+    	}
+    	
+	if(price%hourlyWage){
+    		numHours = Math.floor(price/hourlyWage);
+    	}
+    	
+	x[i].innerHTML = numDays + " Day(s) and " + numHours + " Hours";
+}
