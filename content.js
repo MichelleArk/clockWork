5722a177e2754fdba9e679a545d8b6b67ec5bece
JKereliuk <j.kereliuk@mail.utoronto.ca>
2015-07-07 01:20:03 -0400

    added seconds

--- content.js
+++ content.js
@@ -12,6 +12,7 @@ for(var i = 0; i < x.length; i++){
         var dailyWage = hourlyWage * workingHours;
         var yearlyWage = dailyWage * 251;
     	var minuteWage = hourlyWage / 60;
+        var secondWage = minuteWage / 60;
         var price = Number(res);
 
         // check string converted into a real price value
@@ -19,6 +20,7 @@ for(var i = 0; i < x.length; i++){
             var numYears;
             var numDays;
             var numHours;
+            var numSeconds;
         
             numYears = Math.floor(price/yearlyWage);
             price = price%yearlyWage;
@@ -30,17 +32,24 @@ for(var i = 0; i < x.length; i++){
             price = price%hourlyWage;
 
             numMinutes = Math.floor(price/minuteWage);
+            price = price%minuteWage
+
+            numSeconds = Math.floor(price/secondWage);
      
-            var minutesString = numMinutes + " Minutes";
-                var hoursString = numHours + " Hours";
+            var secondString = numSeconds + " Second(s)"
+            var minutesString = numMinutes + " Minute(s)";
+            var hoursString = numHours + " Hour(s)";
+            var daysString = numDays + " Day(s)"
 
             var outputString;
             if(numDays != 0){
-                outputString = numDays + " Day(s) and " + hoursString;
+                outputString = daysString + " and " + hoursString;
             }else if(numHours != 0){
                 outputString = hoursString + " and " + minutesString;
+            }else if(numMinutes != 0){
+                outputString = minutesString + " and " + secondString;
             }else{
-                outputString = minutesString;
+                outputString = secondString;
             }
             x[i].innerHTML = outputString;
         }
